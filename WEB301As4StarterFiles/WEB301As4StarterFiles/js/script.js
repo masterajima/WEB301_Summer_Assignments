6c082de227b502d2925ed980ae4c6963ba5345f4
Ajima Master <98704309+masterajima@users.noreply.github.com>
2022-07-15 17:02:22 -0400

    to check browser support geolocation

--- WEB301As4StarterFiles/WEB301As4StarterFiles/js/script.js
+++ WEB301As4StarterFiles/WEB301As4StarterFiles/js/script.js
@@ -3,7 +3,20 @@
 */
 add a click event handler for the first button
 $('document').ready(function(){
-  $('#position').on('click',function(){
-
+  $('#position').on('click',getLocation)
+function getLocation(){
+    //check geo location is available in browser or not
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+      document.getElementById("longitude").innerHTML = "Geolocation is ";
+      }
+     }
+     function showPosition(position) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+      document.getElementById("latitude").innerHTML = "Latitude: " +
+     position.coords.latitude +
+      "<br>Longitude: " + position.coords.longitude;
+     }
+    
   });
-});
